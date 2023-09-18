1df7996f9e18053afaeb1dcfa109a811e24a753f
Wambui Ndungu <wambuin18@gmail.com>
2023-09-18 12:09:45 +0300

    Form css adjustments

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
-import { Contact } from '@/components'
+"use client";
+
+import { Form, Hero } from '@/components'
 
 export default function ContactPage() {
   return (
-    <div className='pb-12'>
-       <Contact />
+    <div className=''>
+       <Hero
+        Heading="Contact Us"
+        Paragraph="For any additional information or if you're interested in partnering with us, please don't hesitate to get in touch."
+      />
+      <Form />
     </div>
   );
 }
